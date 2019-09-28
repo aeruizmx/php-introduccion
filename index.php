d64d8a8960dbeb29b7a865706fabccc6a96f33ea
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-09-28 16:33:54 -0500

    Sintaxis de PHP

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1>Hector Benitez</h1>
+        <h1><?php echo "ANDRES RUIZ"; ?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="aec6cbcddac1dceec3cfc7c280cdc1c3">[email&#160;protected]</a></li>
