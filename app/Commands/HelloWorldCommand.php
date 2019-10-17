dbb4a8ebebc98c39b491ef791409a9e6b8ab8a11
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-17 15:02:40 -0500

    Comandos

---
+++ app/Commands/HelloWorldCommand.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelloWorldCommand extends Command{
+    protected static $defaultName = 'app:hello-world';
+
+    protected function configure()
+    {
+        $this ->addArgument('name', InputArgument::REQUIRED, 'Name of the user.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln( 'Hello '. $input->getArgument('name'));
+    }
+}
